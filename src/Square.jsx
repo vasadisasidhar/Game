8d5579c5fdb88c34910b8ed67523fb96077236d6
vasadisasidhar <vasadi.sasidhar92@gmail.com>
2024-08-19 22:30:49 +0530

    First Commit

---
+++ src/Square.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+const Square = ({value, onClick}) => {
+  return (
+    <div>
+          <button className="square" onClick={onClick}>
+              {value}
+          </button>
+    </div>
+  )
+}
+
+export default Square
